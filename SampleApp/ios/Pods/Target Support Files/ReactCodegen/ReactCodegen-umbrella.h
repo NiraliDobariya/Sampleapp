beae802a7974ad9ff74011f1a97c062810dae2d9
Nirali Dobariya <nirali.dobariya@pyther.com>
2024-09-18 16:50:54 +0530

    [Added pods]

---
+++ SampleApp/ios/Pods/Target Support Files/ReactCodegen/ReactCodegen-umbrella.h
@@ -0,0 +1,45 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "FBReactNativeSpec/FBReactNativeSpec.h"
+#import "FBReactNativeSpecJSI.h"
+#import "RCTModulesConformingToProtocolsProvider.h"
+#import "react/renderer/components/rngesturehandler_codegen/ComponentDescriptors.h"
+#import "react/renderer/components/rngesturehandler_codegen/EventEmitters.h"
+#import "react/renderer/components/rngesturehandler_codegen/Props.h"
+#import "react/renderer/components/rngesturehandler_codegen/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rngesturehandler_codegen/ShadowNodes.h"
+#import "react/renderer/components/rngesturehandler_codegen/States.h"
+#import "react/renderer/components/rnpicker/ComponentDescriptors.h"
+#import "react/renderer/components/rnpicker/EventEmitters.h"
+#import "react/renderer/components/rnpicker/Props.h"
+#import "react/renderer/components/rnpicker/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnpicker/ShadowNodes.h"
+#import "react/renderer/components/rnpicker/States.h"
+#import "react/renderer/components/rnskia/ComponentDescriptors.h"
+#import "react/renderer/components/rnskia/EventEmitters.h"
+#import "react/renderer/components/rnskia/Props.h"
+#import "react/renderer/components/rnskia/RCTComponentViewHelpers.h"
+#import "react/renderer/components/rnskia/ShadowNodes.h"
+#import "react/renderer/components/rnskia/States.h"
+#import "rnasyncstorage/rnasyncstorage.h"
+#import "rnasyncstorageJSI.h"
+#import "rngesturehandler_codegen/rngesturehandler_codegen.h"
+#import "rngesturehandler_codegenJSI.h"
+#import "rnreanimated/rnreanimated.h"
+#import "rnreanimatedJSI.h"
+#import "rnskia/rnskia.h"
+#import "rnskiaJSI.h"
+
+FOUNDATION_EXPORT double ReactCodegenVersionNumber;
+FOUNDATION_EXPORT const unsigned char ReactCodegenVersionString[];
+
